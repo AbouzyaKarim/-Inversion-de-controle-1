f3b4c6f4927e66965dceb27a31f33fdd32f32139
AbouzyaKarim <kabouzya@gmail.com>
2022-04-29 11:44:44 +0000

    FC

---
+++ src/dao/DaoImpl.java
@@ -0,0 +1,14 @@
+package dao;
+
+public class DaoImpl implements IDao{
+    @Override
+    public double getData() {
+
+        /*
+        Se connecter à la BD pour récuperer l'information
+         */
+        System.out.println("Version de base de donnée");
+        double temp=Math.random()*40;
+        return temp;
+    }
+}
---
+++ src/pres/Presentation.java
@@ -0,0 +1,20 @@
+package pres;
+
+import dao.DaoImpl;
+import ext.DaoImpl2;
+import metier.MetierImpl;
+
+public class Presentation {
+
+    public static void main(String[] args) {
+        /*
+        Injection des dépendances par
+        instanciation statique ==> new
+        Couplage fort
+         */
+        DaoImpl2 dao = new DaoImpl2();
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(dao);
+        System.out.println("Resultat = "+metier.calcul());
+    }
+}
